573c27f21256a1f9ccd66a4793a50f2602bff2f0
Saty4Dev <57029286+Saty4Dev@users.noreply.github.com>
2022-01-31 17:02:09 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,19 @@
 // import using require
 
 // declare class
+class Circle extends Shape{
+constructor(radius,colour,name){
+super(radius,radius,colour,name);
+this.radius=radius;
+this.colour=colour;
+this.name=name;
+}
+ calculateArea() { var area=2*3.14*this.radius;
+console.log("the area is"+area);}
+}
+let Cr= new Circle(2,2,'blue','circle');
+Cr.calculateArea();
+module.exports= Circle;
 
 
 // export class using module.exports
