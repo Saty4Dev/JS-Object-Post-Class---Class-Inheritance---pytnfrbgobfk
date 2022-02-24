8f5e3834d9b636b75a42fb4653550d3554de687a
Saty4Dev <57029286+Saty4Dev@users.noreply.github.com>
2022-02-25 02:19:02 +0530

    Assignment

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,22 @@
-// import using require
+require("./Shape");
 
 // declare class
+class Circle {
+  constructor(radius, colour, name) {
+    super(radius, colour, name);
+    this.radius = radius;
+    this.colour = colour;
+    this.name = name;
+  }
 
+  calculateArea() {
+    var area = 2 * 3.14 * this.radius;
+    console.log("the area is" + area);
+  }
+}
+
+let Cr = new Circle(2, "blue", "circle");
+Cr.calculateArea();
+module.exports(Circle);
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,20 @@
-// declare class
+class Shape {
+  constructor(length, breadth, colour, name) {
+    this.length = length;
+    this.breadth = breadth;
+    this.colour = colour;
+    this.name = name;
+  }
 
+  drawShape() {
+    return "The shape" + this.name + "is drawn";
+  }
+
+  calculateArea() {
+    var area1 = this.length * this.breadth;
+    console.log("the area is" + area1);
+  }
+}
+
+module.exports(Shape);
 // export class using module.exports
